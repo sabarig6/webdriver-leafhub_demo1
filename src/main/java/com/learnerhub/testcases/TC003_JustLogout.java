16424a44ad90bfee414c43e1a08303604a8682d5
yaswaantr <yaswaantr@gmail.com>
2024-09-15 16:51:23 +0530

    TC03 - line 5 to line 10 updated

--- src/main/java/com/learnerhub/testcases/TC003_JustLogout.java
+++ src/main/java/com/learnerhub/testcases/TC003_JustLogout.java
@@ -1,5 +1,12 @@
 package com.learnerhub.testcases;
 
 public class TC003_JustLogout {
+	
+	public static void main(String[] args) 
+    { 
+        System.out.println("Welcome"); 
+        System.out.println("To"); 
+        System.out.println("GeeksforGeeks"); 
+    } 
 
 }
